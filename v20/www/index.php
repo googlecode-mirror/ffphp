fbde8ba675d5729efa38b28fc69caafbababdd94
tongseo@gmail.com <tongseo@gmail.com@118006fc-11ee-54f6-6777-0a7d87ea3571>
2012-11-29 02:52:36 +0000

    细节更新

--- v20/www/index.php
+++ v20/www/index.php
@@ -6,5 +6,12 @@ function mf()
 }
 $start_time = mf();
 
+
 define('SYS_PATH',dirname(dirname(__FILE__)).'/');
 require SYS_PATH.'/init.php';
+
+//多项目时这里判断
+//if():
+	define ('SYS_NAME','app');
+	define ('SYS_APP',SYS_PATH.SYS_NAME.'/');
+//endif;
